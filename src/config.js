ed2e6f9901495e20165279ec289a62a1a4af788c
Rafael Jesus <rafaelljesus86@gmail.com>
2016-05-20 19:45:56 -0300

    Change urls

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 const host = process.env.MONGO_PORT_27017_TCP_ADDR || 'localhost'
-const port = process.env.MONGO_PORT_27017_TCP_PORT || 27017
 
 export default {
-  test: `mongodb://${host}:${port}/events_test`,
-  development: `mongodb://${host}:${port}/events`,
+  test: `mongodb://localhost:27017/events_test`,
+  development: `mongodb://${host}:27017/events`,
   production: process.env.MONGODB_URL
 }
